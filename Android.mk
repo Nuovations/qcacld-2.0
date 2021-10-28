24c4c7887335f1e21c7de3e73227239e6d769d0e
David Escalona <david.escalona@digi.com>
2021-10-28 13:21:09 +0200

    Android.mk: adapt android makefile to Oreo version
    https://onedigi.atlassian.net/browse/ADK4A-1900
    
    Signed-off-by: David Escalona <david.escalona@digi.com>

--- Android.mk
+++ Android.mk
@@ -1,19 +1,18 @@
-ifeq ($(BOARD_HAVE_WIFI_QCA6574),true)
+ifeq ($(BOARD_HAVE_WIFI_QCA6564),true)
 
 LOCAL_PATH := $(call my-dir)
 ANDROID_ROOT := $(PWD)
 DRIVER_SRC_BASE := $(LOCAL_PATH)
 LOCAL_PATH_BACKUP := $(ANDROID_ROOT)/$(LOCAL_PATH)
 
-ifeq ($(TARGET_ARCH),arm64)
-CROSS_COMPILE := $(ANDROID_ROOT)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-else
-CROSS_COMPILE := $(ANDROID_ROOT)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-endif
+KERNEL_TOOLCHAIN_ABS := $(realpath prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin)
+KERNEL_CROSS_COMPILE := $(KERNEL_TOOLCHAIN_ABS)/arm-linux-androideabi-
+
+KERNEL_CROSS_COMPILE_WRAPPER := $(realpath $(CC_WRAPPER)) $(KERNEL_CROSS_COMPILE)
 
 # Generic flags
-MAKE_OPTIONS := ARCH=$(TARGET_ARCH)
-MAKE_OPTIONS += CROSS_COMPILE=$(CROSS_COMPILE)
+MAKE_OPTIONS := ARCH=arm
+MAKE_OPTIONS += CROSS_COMPILE="$(KERNEL_CROSS_COMPILE_WRAPPER)"
 MAKE_OPTIONS += WLAN_ROOT=$(LOCAL_PATH_BACKUP)
 MAKE_OPTIONS += MODNAME=wlan
 MAKE_OPTIONS += WLAN_OPEN_SOURCE=1
@@ -26,11 +25,9 @@ MAKE_OPTIONS += CONFIG_NON_QC_PLATFORM=y
 MAKE_OPTIONS += CONFIG_LINUX_QCMBR=y
 MAKE_OPTIONS += BUILD_DEBUG_VERSION=0
 
-# Custom flags to compile PCI version
-MAKE_OPTIONS += CONFIG_ROME_IF=pci
-MAKE_OPTIONS += CONFIG_HIF_PCI=1
-MAKE_OPTIONS += CONFIG_ATH_11AC_TXCOMPACT=1
-MAKE_OPTIONS += CONFIG_ATH_PCIE_MAX_PERF=1
+# Custom flags to compile SDIO version
+MAKE_OPTIONS += CONFIG_ROME_IF=sdio
+MAKE_OPTIONS += CONFIG_CLD_HL_SDIO_CORE=y
 
 KERNEL_SRC := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
 
@@ -38,23 +35,23 @@ QCACLD_INTERMEDIATES := $(TARGET_OUT_INTERMEDIATES)/$(LOCAL_MODULE_CLASS)/$(LOCA
 
 # Override the default build target in order to issue our own custom command.
 # Note that the module name is wlan.ko by default, we then change it to
-# qca6574_wlan.ko in order to be more explicit.
+# qca6564_wlan.ko in order to be more explicit.
 $(DRIVER_SRC_BASE)/wlan.ko: $(mod_cleanup) bootimage | $(ACP)
 	$(MAKE) -C $(KERNEL_SRC) M=$(LOCAL_PATH_BACKUP) $(MAKE_OPTIONS) modules
-	$(hide) $(CROSS_COMPILE)strip --strip-debug $(LOCAL_PATH_BACKUP)/wlan.ko
+	$(hide) $(KERNEL_CROSS_COMPILE_WRAPPER)strip --strip-debug $(LOCAL_PATH_BACKUP)/wlan.ko
 	$(hide) mkdir -p $(QCACLD_INTERMEDIATES)
-	$(hide) $(ACP) $(LOCAL_PATH_BACKUP)/wlan.ko $(QCACLD_INTERMEDIATES)/qca6574_wlan.ko
+	$(hide) $(ACP) $(LOCAL_PATH_BACKUP)/wlan.ko $(QCACLD_INTERMEDIATES)/qca6564_wlan.ko
 
 mod_cleanup := $(ANDROID_ROOT)/$(DRIVER_SRC_BASE)/dummy
 
 $(mod_cleanup):
 	$(MAKE) -C $(KERNEL_SRC) M=$(PWD) clean
 
-qca6574_clean: $(mod_cleanup)
+qca6564_clean: $(mod_cleanup)
 
 # wlan.ko
 include $(CLEAR_VARS)
-LOCAL_MODULE       := qca6574_wlan.ko
+LOCAL_MODULE       := qca6564_wlan.ko
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/lib/modules/
 LOCAL_MODULE_CLASS := ETC
